fd59fb36c49047456ef35ed98d012e6f0e39dc05
Ryan Tran <ryantran@vpn-172-27-141-39.host.ucla.edu>
2020-11-23 07:43:37 -0800

    Configured Airtable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Airtable from 'airtable';
+
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
+const base = new Airtable({ apiKey: airtableConfig.apiKey }).base(airtableConfig.baseKey);
 
 function App() {
+  useEffect(() => {
+    base('Farms')
+      .select({ view: 'Grid view' })
+      .eachPage((records, fetchNextPage) => {
+        console.log(records);
+        fetchNextPage();
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Farm2People
+      </p>
     </div>
   );
 }

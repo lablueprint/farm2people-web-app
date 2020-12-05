eb8b50cfb5da9fa5e6c7ba1730c10da79c2d83a8
DiyaBaliga <baligadiya@gmail.com>
2020-12-04 18:04:28 -0800

    Airtable call setup

--- src/components/AddFarm/AddFarm.js
+++ src/components/AddFarm/AddFarm.js
@@ -1,20 +1,67 @@
-import { TextField } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
 import React from 'react';
 import './AddFarm.css';
+
+// Airtable set-up here
+const Airtable = require('airtable');
+
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY })
+  .base(process.env.REACT_APP_AIRTABLE_BASE_KEY);
 
 export default class AddFarm extends React.Component {
   constructor(props) {
     super(props);
-    console.log('i am the add farm constructor');
+    console.log('constructor sb printed');
     this.state = {
       farmName: null,
       description: null,
-      /* userID: null, // this.props.userID? */
+      /* userID: null, */
       email: null,
       phone: null,
       address: null,
       zipCode: null,
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const {
+      farmName, description, email, phone, address, zipCode,
+    } = this.state;
+    base('Farms').create([
+      {
+        fields: {
+          'farm name': farmName,
+          description,
+          email,
+          phone,
+          address,
+          'zip code': zipCode,
+        },
+      },
+    ], (err, records) => {
+      if (err) {
+        console.error(`error: ${err}`);
+        return;
+      }
+      records.forEach((record) => {
+        console.log(`record id: ${record.getId()}`);
+      });
+    });
+    /*  .then(
+        this.setState({ // Reset state to initial blank values
+          farmName: null,
+          description: null,
+          /* userID: null,
+          email: null,
+          phone: null,
+          address: null,
+          zipCode: null,
+        }),
+      ); */
+    console.log(`user submitted farm info'${farmName || 'null'}`);
   }
 
   render() {
@@ -23,7 +70,7 @@ export default class AddFarm extends React.Component {
     } = this.state;
 
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h1>
           {farmName || 'no farm name :('}
           {description || 'no description :('}
@@ -36,6 +83,7 @@ export default class AddFarm extends React.Component {
           <TextField
             id="outlined-basic"
             label="Enter your farm name"
+            value={farmName || ''}
             required
             onChange={(event) => this.setState({ farmName: event.target.value })}
           />
@@ -44,6 +92,7 @@ export default class AddFarm extends React.Component {
           <TextField
             id="outlined-multiline-flexible"
             label="Enter your description"
+            value={description || ''}
             multiline
             rowsMax={4}
             onChange={(event) => this.setState({ description: event.target.value })}
@@ -52,6 +101,7 @@ export default class AddFarm extends React.Component {
         <TextField
           id="outlined-basic"
           label="Enter your email"
+          value={email || ''}
           required
           onChange={(event) => this.setState({ email: event.target.value })}
         />
@@ -60,7 +110,7 @@ export default class AddFarm extends React.Component {
           <TextField
             id="outlined-basic"
             label="Enter your phone number"
-            required
+            value={phone || ''}
             onChange={(event) => this.setState({ phone: event.target.value })}
           />
         </div>
@@ -68,7 +118,7 @@ export default class AddFarm extends React.Component {
           <TextField
             id="outlined-basic"
             label="Enter your address"
-            required
+            value={address || ''}
             onChange={(event) => this.setState({ address: event.target.value })}
           />
         </div>
@@ -76,9 +126,17 @@ export default class AddFarm extends React.Component {
           <TextField
             id="outlined-basic"
             label="Enter your zip code"
-            required
+            value={zipCode || ''}
             onChange={(event) => this.setState({ zipCode: event.target.value })}
           />
+        </div>
+        <div>
+          <Button
+            type="submit"
+            value="submit"
+          >
+            Submit!
+          </Button>
         </div>
       </form>
     );

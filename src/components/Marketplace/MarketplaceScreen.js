d88656372638c079caa91b6d9b51981a76eaf391
Helen Wang <helenjwang9@gmail.com>
2021-01-14 00:22:58 -0800

    Add prelim tab navigation

--- src/components/Marketplace/MarketplaceScreen.js
+++ src/components/Marketplace/MarketplaceScreen.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  AppBar, IconButton, InputBase, Toolbar,
+  AppBar, IconButton, InputBase, Tab, Tabs, Toolbar,
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import './MarketplaceScreen.css';
 
 export default function MarketplaceScreen() {
+  // 3 tab states: 0 (purchase buy), 1 (aggregate buy), 2 (distressed buy)
+  const [currentTab, setCurrentTab] = useState(0);
   return (
     <div>
       <h1> Marketplace Screen :) </h1>
@@ -35,7 +37,23 @@ export default function MarketplaceScreen() {
           </Toolbar>
         </AppBar>
       </div>
-      {/* 3-tab buy overview of individual farm listings */}
+      {/* 3-tab buy overview of individual farm listings  */}
+      <div className="tabContainer">
+        <AppBar position="static">
+          <Tabs
+            value={currentTab}
+            onChange={(event, newTab) => setCurrentTab(newTab)}
+            aria-label="Purchase Buy, Aggregate Buy, Distressed Buy tab navigation"
+          >
+            <Tab label="Purchase Buy" />
+            <Tab label="Aggregate Buy" />
+            <Tab label="Distressed Buy" />
+          </Tabs>
+        </AppBar>
+        <h1>
+          {currentTab}
+        </h1>
+      </div>
     </div>
   );
 }

5fedc29393c7fcc77e11ebabc77bfedcaf4246bc
Helen Wang <helenjwang9@gmail.com>
2021-01-13 23:36:17 -0800

    Prelim (v ugly) search bar on marketplace

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import AddFarm from '../AddFarm';
 
 export default function HomeScreen() {
   return (
     <div>
       <h1> Home Screen </h1>
-      <AddFarm />
     </div>
   );
 }
--- src/components/Marketplace/MarketplaceScreen.js
+++ src/components/Marketplace/MarketplaceScreen.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import {
+  AppBar, IconButton, InputBase, Toolbar,
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import './MarketplaceScreen.css';
 
 export default function MarketplaceScreen() {
-  return <h1>Marketplace Screen</h1>;
+  return (
+    <div>
+      <h1> Marketplace Screen :) </h1>
+      {/* Search bar element */ }
+      <div className="searchBarContainer">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="drop down menu"
+            >
+              <ArrowDropDownIcon />
+            </IconButton>
+            <InputBase
+              placeholder="Search here"
+              color="primary"
+            />
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="search icon"
+            >
+              <SearchIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+      {/* 3-tab buy overview of individual farm listings */}
+    </div>
+  );
 }

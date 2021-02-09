6a76003047b4334e5bc0ee09cf1027da9a528eb8
Ryan Tran <ryantran@Ryans-MacBook-Pro.local>
2021-02-09 00:39:10 -0800

    Refactored Components and Emulated Navbar Figma Styles

---
+++ src/components/Cart/CartScreen.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CartScreen() {
+  return <h1>Cart Screen</h1>;
+}
---
+++ src/components/Profile/ProfileScreen.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ProfileScreen() {
+  return <h1>Profile Screen</h1>;
+}

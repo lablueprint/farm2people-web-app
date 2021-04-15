25f5e99e47adea21268f2476733befc8adc20857
Raphael Gatchalian <raphaelg09@ucla.edu>
2021-04-14 21:58:37 -0700

    concretized front end for changepassword component

--- src/components/Contact/ChangePassword.js
+++ src/components/Contact/ChangePassword.js
@@ -1,35 +1,72 @@
 import React from 'react';
 import {
-  TextField, Typography, Dialog, Box, Button,
+  TextField, FormControl, Typography, Dialog, Box, Button,
 } from '@material-ui/core';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  makeStyles, withStyles,
+} from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles({
-  container: {
-    background: '#ffaaaa',
-  },
   box: {
-    background: '#aaffaa',
+    // background: '#aaffaa',
   },
   leftHeading: {
     fontFamily: 'Work Sans',
     fontSize: '28px',
     fontWeight: '700',
     color: '#373737',
-    // marginTop: '2.8%',
-    // marginBottom: '2.5%',
     textAlign: 'left',
+  },
+  form: {
+    // background: 'pink',
+    width: '400px',
+    fontFamily: 'Work Sans',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+  },
+  formControl: {
+    margin: '10px',
+  },
+  inputLabel: {
+    fontFamily: 'Work Sans',
+    fontWeight: '600',
+    color: '#373737',
+    '&:after': {
+      content: '" *"',
+      color: '#f00',
+    },
+  },
+  placeholder: {
+    fontFamily: 'Work Sans',
+  },
+  changePasswordButton: {
+    fontFamily: 'Work Sans',
+  },
+  cancelButton: {
+    fontFamily: 'Work Sans',
+    textTransform: 'none',
+    fontWeight: '600',
   },
   centerAlignDialogActions: {
     justifyContent: 'center',
   },
 });
+
+const ChangePasswordButton = withStyles({
+  root: {
+    color: '#fff',
+    backgroundColor: '#53AA48',
+    '&:hover': {
+      backgroundColor: '#3D7736',
+    },
+  },
+})(Button);
 
 export default function AddListing() {
   const classes = useStyles();
@@ -44,18 +81,18 @@ export default function AddListing() {
   };
 
   return (
-    <Box m={3} p={3} className={classes.box}>
+    <Box m={2} p={2} className={classes.box}>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Open form dialog
       </Button>
       <Dialog
         open={open}
         onClose={handleClose}
-        maxWidth="md"
+        maxWidth="sm"
         fullWidth
       >
-        <Box p={5}>
-          <DialogTitle id="form-dialog-title">
+        <Box p={3}>
+          <DialogTitle>
             <Box display="flex" justifyContent="space-between">
               <Typography align="center" className={classes.leftHeading}>
                 Change Password
@@ -66,19 +103,61 @@ export default function AddListing() {
             </Box>
           </DialogTitle>
           <DialogContent>
-            <Box className={classes.box}>
-              <TextField />
-              TODO: Add form here
+            <Box p={6} pt={4} pb={6} className={classes.box} justifyContent="center">
+              <form className={classes.form} noValidate>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <Typography className={classes.inputLabel}>
+                    Old Password
+                  </Typography>
+                  <TextField
+                    variant="outlined"
+                    type="password"
+                    placeholder="Enter your old password"
+                    InputProps={{ className: classes.placeholder }}
+                    margin="dense"
+                    required
+                  />
+                </FormControl>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <Typography className={classes.inputLabel}>
+                    New Password
+                  </Typography>
+                  <TextField
+                    variant="outlined"
+                    type="password"
+                    placeholder="Enter a new password"
+                    InputProps={{ className: classes.placeholder }}
+                    margin="dense"
+                    required
+                  />
+                </FormControl>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <Typography className={classes.inputLabel}>
+                    Confirm New Password
+                  </Typography>
+                  <TextField
+                    variant="outlined"
+                    type="password"
+                    placeholder="Confirm your new password"
+                    InputProps={{ className: classes.placeholder }}
+                    margin="dense"
+                    required
+                  />
+                </FormControl>
+              </form>
             </Box>
           </DialogContent>
           <DialogActions classes={{ root: classes.centerAlignDialogActions }}>
-
-            <Button onClick={handleClose} color="primary">
-              Cancel
+            <Button onClick={handleClose}>
+              <Typography className={classes.cancelButton}>
+                Cancel
+              </Typography>
             </Button>
-            <Button onClick={handleClose} color="primary">
-              Change Password
-            </Button>
+            <ChangePasswordButton variant="contained" size="large" onClick={handleClose}>
+              <Typography className={classes.changePasswordButton}>
+                CHANGE PASSWORD
+              </Typography>
+            </ChangePasswordButton>
           </DialogActions>
         </Box>
       </Dialog>

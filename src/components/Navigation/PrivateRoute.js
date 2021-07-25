b98b887e12d180c05418cb1b628a8883bb76c13c
ryanmtran <ryanmtran922@gmail.com>
2021-07-24 23:26:24 -0700

    Added userflows and registration integration

--- src/components/Navigation/PrivateRoute.js
+++ src/components/Navigation/PrivateRoute.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {
+  Grid,
+} from '@material-ui/core';
 import { store } from '../../lib/redux/store';
 
-const PrivateRoute = ({ component: Component, allowedRoles, ...rest }) => (
+const PrivateRoute = ({
+  component: Component, allowedRoles, loading, approvalPermissions, ...rest
+}) => (
   <Route
     {...rest}
     render={(props) => {
       /* User must be authenticated and be part of the allowedRules to access
       a PrivateRoute */
-      if (store.getState().authenticated
+      if ((loading || approvalPermissions === null) && store.getState().authenticated) {
+        return (
+          <Grid
+            container
+            spacing={0}
+            align="center"
+            justify="center"
+            direction="column"
+            style={{ height: '100vh' }}
+          >
+            <Grid item>
+              <CircularProgress align="center" />
+            </Grid>
+          </Grid>
+        );
+      }
+      if (store.getState().authenticated && approvalPermissions
         && allowedRoles.includes(store.getState().userData.user.fields['user type'])) {
         return <Component {...props} />;
       }
@@ -32,5 +54,7 @@ export default PrivateRoute;
 PrivateRoute.propTypes = {
   component: PropTypes.elementType.isRequired,
   allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+  loading: PropTypes.bool.isRequired,
+  approvalPermissions: PropTypes.bool.isRequired,
 
 };

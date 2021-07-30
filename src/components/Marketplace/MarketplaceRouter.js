2842807c88bee10660560a56e072382d50bbdd4c
DiyaBaliga <baligadiya@gmail.com>
2021-07-29 22:59:16 -0700

    search bug fix

--- src/components/Marketplace/MarketplaceRouter.js
+++ src/components/Marketplace/MarketplaceRouter.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
-import { history } from '../../lib/redux/store';
+import { history, store } from '../../lib/redux/store';
 import MarketplaceScreen from './MarketplaceScreen';
 import { base } from '../../lib/airtable/airtable';
 
@@ -16,14 +16,27 @@ export default function MarketplaceRouter() {
   }
 
   // Get prod id, grouped unit type, price per grouped unit for each produce record
+  const getInitialRole = () => (store.getState().userData == null ? '' : store.getState().userData.user.fields['user type']);
+  // Get prod id, grouped unit type, price per grouped unit for each produce record
   function getProduceRecords() {
     base('Listings').select({ view: 'Grid view' }).all()
       .then((produceRecords) => {
         const processedProduceRecords = [];
         produceRecords.forEach((record) => {
+          // If this record is private, skip it so it's not displayed
+          if (record.fields.privatized) {
+            return;
+          }
           const pricePerGroup = record.fields['standard price per grouped produce type'] || 0;
+          const agencyPricePerGroup = record.fields['agency price per grouped produce type'] || 0;
           const groupPerPallet = record.fields['grouped produce type per pallet'] || 0;
-          const palletPrice = pricePerGroup * groupPerPallet;
+          let palletPrice = pricePerGroup * groupPerPallet;
+          let hasAgencyPrice = false;
+          const userRole = getInitialRole();
+          if (userRole === 'agency' && agencyPricePerGroup < pricePerGroup) {
+            hasAgencyPrice = true;
+            palletPrice = agencyPricePerGroup * groupPerPallet;
+          }
           // Capitalise 1st letter of produce type to match filtering
           let produceType = record.fields['produce category'] ? record.fields['produce category'][0] : 'No category';
           produceType = produceType.charAt(0).toUpperCase() + produceType.slice(1);
@@ -31,10 +44,17 @@ export default function MarketplaceRouter() {
             listingID: record.fields['listing id'],
             produceID: record.fields.produce ? record.fields.produce[0] : -1,
             farmID: record.fields['farm id'],
-            palletPrice: palletPrice !== 0 ? palletPrice : -1,
+            palletPrice,
             season: record.fields['growing season'] || 'No season',
             produceType,
             palletsAvailable: record.fields['pallets available'] || 0,
+            hasAgencyPrice,
+            // fields for sorting
+            'sell by date': record.fields['sell by date'] || 0,
+            'first available date': record.fields['first available date'] || 0,
+            'available until': record.fields['available until'] || 0,
+            'date entered': record.fields['data entered'] || 0,
+            'produce name': record.fields['produce name'] || 'No name',
           };
           processedProduceRecords.push(recordInfo);
         });
--- src/components/Marketplace/Sidebar/MarketplaceSidebar.js
+++ src/components/Marketplace/Sidebar/MarketplaceSidebar.js
@@ -40,8 +40,11 @@ const useStyles = makeStyles({
 
 /* Sidebar with filter selections for types + sorting */
 export default function MarketplaceSidebar({
-  prodTypeFilters, itemsPerProdType, onProduceFilterChange, priceOptions, itemsPerPrice,
-  onPriceFilterChange, farmSeasonFilters, itemsPerFarmSeason, onSeasonFilterChange,
+  updateSortOrder,
+  itemTypeFilters, itemsPerItemType, onItemFilterChange,
+  prodTypeFilters, itemsPerProdType, onProduceFilterChange,
+  priceOptions, itemsPerPrice, onPriceFilterChange,
+  farmSeasonFilters, itemsPerFarmSeason, onSeasonFilterChange,
 }) {
   const classes = useStyles();
 
@@ -51,9 +54,23 @@ export default function MarketplaceSidebar({
       <Typography className={classes.filterText}>
         FILTERS
       </Typography>
-      <SortMenu sortOptions={['Sell by date: earliest first', 'First available date: earliest first', 'Available until: earliest first', 'Recently added first', 'Alphabetical order']} />
-      {/* TODO: Item type menu only shows for agency buyer view */}
-      <FilterMenu menuTitle="Item Type" filterOptions={['Agency Price', 'Standard Items']} isLast={false} />
+      <SortMenu
+        sortOptions={[
+          { label: 'Sell by date: earliest first', target: 'sell by date' },
+          { label: 'First available date: earliest first', target: 'first available date' },
+          { label: 'Available until: earliest first', target: 'available until' },
+          { label: 'Recently added first', target: 'date entered' },
+          { label: 'Alphabetical order', target: 'produce name' },
+        ]}
+        updateSortOrder={updateSortOrder}
+      />
+      <FilterMenu
+        menuTitle="Item Type"
+        filterOptions={itemTypeFilters}
+        itemsPerOption={itemsPerItemType}
+        onFilterChange={onItemFilterChange}
+        isLast={false}
+      />
       <FilterMenu
         menuTitle="Produce Type"
         filterOptions={prodTypeFilters}
@@ -78,6 +95,10 @@ export default function MarketplaceSidebar({
 }
 
 MarketplaceSidebar.propTypes = {
+  updateSortOrder: PropTypes.func.isRequired,
+  itemTypeFilters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onItemFilterChange: PropTypes.func.isRequired,
+  itemsPerItemType: PropTypes.arrayOf(PropTypes.number).isRequired,
   prodTypeFilters: PropTypes.arrayOf(PropTypes.string).isRequired,
   itemsPerProdType: PropTypes.arrayOf(PropTypes.number).isRequired,
   onProduceFilterChange: PropTypes.func.isRequired,
